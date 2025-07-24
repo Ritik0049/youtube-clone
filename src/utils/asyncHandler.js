3ea7fbe48cfb7527570f762f34325d65a374249b
Ritik Bhardwaj <ritikrb2004@gmail.com>
2025-07-24 17:53:18 +0530

    utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,46 @@
+const asyncHandler = (reqHandler) => {
+     (req, res, next) => {
+       Promise.resolve(reqHandler(req, res, next))
+       .catch((err) => next(err))
+    }   
+}
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//   try {
+//     await fn(req, res, next)
+//   } catch (error) {
+//      res.status(error.code || 500).json({
+//         success: false,
+//         messsage: error.messsage
+//      })
+//   }
+// }
